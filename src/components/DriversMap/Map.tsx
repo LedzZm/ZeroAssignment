be2d100aa3f8ccca8925283ed6869171d08e7aec
Marios Anagnostopoulos <m.anagnostopoulos@netstudio.gr>
2020-09-25 04:31:31 +0300

    Amends 550a2ee.

--- src/components/DriversMap/Map.tsx
+++ src/components/DriversMap/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import { DriverNetwork } from "./DriverNetwork";
 
